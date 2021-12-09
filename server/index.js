84b3f25d5e7ab4255c95f997d839815fa7b3cca0
vrunt <hiimvrunt@gmail.com>
2021-12-09 16:57:01 -0500

    added sockets

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = http.createServer(app);
-const io = socketio(server, { cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] } });
+const io = socketio(server, { cors: { methods: ['GET', 'POST'] } });
 
 io.on('connection', (socket) => {
     console.log(`new client connected`);
